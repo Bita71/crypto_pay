053ea83fb148f8794e8122a6cf9a9467a619410b
dsamsonov <dsamsonov@ubic.tech>
2023-10-01 16:40:18 +0300

    correct design

--- src/shared/ui/Button/index.tsx
+++ src/shared/ui/Button/index.tsx
@@ -1,26 +1,22 @@
-import React, 
-{ 
-  FC, 
-  HTMLAttributes, 
-} from 'react';
-import classNames from 'classnames';
-import styles from './styles.module.scss';
-import { Text } from '../Text';
+import React, { FC, HTMLAttributes } from "react";
+import classNames from "classnames";
+import styles from "./styles.module.scss";
+import { Text } from "../Text";
 
 interface Props extends HTMLAttributes<HTMLButtonElement> {
-  variant?: 'primary' | 'secondary',
-  color?: 'light' | 'dark',
+  variant?: "primary" | "secondary" | "icon";
+  color?: "light" | "dark";
 }
 
-export const Button: FC<Props>  = function Button({
-  color = 'light',
-  variant = 'primary',
+export const Button: FC<Props> = function Button({
+  color = "light",
+  variant = "primary",
   children,
   className,
   ...props
 }) {
   return (
-    <button 
+    <button
       className={classNames(
         styles.button,
         styles[variant],
@@ -29,18 +25,18 @@ export const Button: FC<Props>  = function Button({
       )}
       {...props}
     >
-      <Text 
-        variant='button' 
-        color='white'
-        component='span' 
-        className={classNames(
-          styles.text,
-          styles[variant],
-          styles[color],
-        )}
-      >
-        {children}
-      </Text>
+      {variant === "icon" ? (
+        children
+      ) : (
+        <Text
+          variant="button"
+          color='white'
+          component="span"
+          className={classNames(styles.text, styles[variant], styles[color])}
+        >
+          {children}
+        </Text>
+      )}
     </button>
-  )
+  );
 };

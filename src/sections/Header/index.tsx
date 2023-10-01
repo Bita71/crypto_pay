c9ac21192678b95fddbe8186040509ff5abdedaa
dsamsonov <dsamsonov@ubic.tech>
2023-10-01 16:33:40 +0300

    correct header

--- src/sections/Header/index.tsx
+++ src/sections/Header/index.tsx
@@ -1,26 +1,27 @@
-import React from "react";
-import Image from "next/image";
+'use client'
+import React, { useState } from "react";
+import classNames from "classnames";
 import { Button, Container } from "@/shared/ui";
+import { Theme, useTheme } from "@/shared/hooks";
+import { LogoIcon, MoonIcon, SunIcon } from "@/shared/icons";
 import { Nav } from "../Nav";
 import styles from "./styles.module.scss";
 
 export const Header = function Header() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <header>
       <Container className={styles.header}>
-        <div className={styles.logo}>
-          <Image
-            src="/logo.svg"
-            alt="Crypto bot logo"
-            className={styles.logoImg}
-            fill
-            objectFit='cover'
-          />
-        </div>
+        <LogoIcon className={styles.logo} />
         <Nav className={styles.nav} />
-        <Button variant="secondary">
-          Get started
-        </Button>
+        <div className={styles.buttons}>
+          <Button onClick={toggleTheme} variant="icon">
+            {theme === Theme.light && <SunIcon className={classNames(styles.themeIcon, styles.light)} />}
+            {theme === Theme.dark && <MoonIcon className={classNames(styles.themeIcon, styles.dark)} />}
+          </Button>
+          <Button variant="secondary">Get started</Button>
+        </div>
       </Container>
     </header>
   );

8897129dab1ceaa6217be8d438c0674f5504662b
dsamsonov <dsamsonov@ubic.tech>
2024-02-13 11:56:27 +0300

    correct output dir

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { i18n } = require('./next-i18next.config')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
+  distDir: 'dist',
 
   webpack(config) {
     config.resolve.fallback = {
